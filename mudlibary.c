ce1009d5c684283a195111f8e541967f5bcdb332
MudSplasher <150399924+MudSplasher@users.noreply.github.com>
2024-07-26 05:12:22 -0400

    Create mudlibary.c

---
+++ mudlibary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define mud "mud"
+#define intemud int
+#define mamud main
+#define mudee (
+#define mudll )
+#define mudlo {
+#define semud }
+#define splashmudfor for
+#define mudthinp ;
+#define mudprinting printf
+#define getmudback return
+#define mudddd on_exit
+#define muddddd [
+#define mudddd ]
+#define mudd 0
+#define mudsplash printf("mud")
